ab191bb080e7197d0cf979fdddd81e2d4c8b42fe
lykkell <lykkellit@gmail.com>
2020-12-22 13:43:51 +0200

    app change to product-list

---
+++ src/components/product-list/product-list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './product.css';
+
+const ProductList = () => {
+    return (
+        <>
+            <h1 class='product-list-header d-flex'>
+                The Product List
+            </h1>
+            <h2 class='product-list-account-name d-flex'>
+                The account name is Account1
+            </h2>
+        </>
+    );
+} 
+export default ProductList;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/';
+import ProductList from './components/product-list';
+// import App from './components/app/';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <ProductList />
   </React.StrictMode>,
   document.getElementById('root')
 );
