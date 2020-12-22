c8dbf55f13bc94a8ecf718565df816f53684e08d
lykkell <lykkellit@gmail.com>
2020-12-22 14:44:12 +0200

    product table

--- src/components/product-list/product-list.js
+++ src/components/product-list/product-list.js
@@ -3,13 +3,31 @@ import './product.css';
 
 const ProductList = () => {
     return (
-        <>
-            <h1 class='product-list-header d-flex'>
-                The Product List
-            </h1>
-            <h2 class='product-list-account-name d-flex'>
-                The account name is Account1
-            </h2>
+        <>  <h2 class='d-flex justify-content-end'>NWITT</h2>
+            <div class='product-list-header d-flex justify-content-around'>
+                <h1 class='product-list-title d-flex'>
+                    The Product List
+                </h1>
+                <h2 class='product-list-account-name d-flex'>
+                    The account name is Account1
+                </h2>
+            </div>
+            <h4 class='d-flex justify-content-around'>
+                <div class='product-list-subheader d-flex'>#code</div>
+                <div class='product-list-subheader d-flex'>Colonum-1</div>
+                <div class='product-list-subheader d-flex'>Colonum-2</div>
+                <div class='product-list-subheader d-flex'>Colonum-3</div>
+                <div class='product-list-subheader d-flex'>Colonum-4</div>
+                <div class='product-list-subheader d-flex'>Colonum-5</div>
+            </h4>
+            <div class='product-list-item d-flex justify-content-around'>
+                <div class='d-flex'>#00001</div>
+                <div class='d-flex'>item-short-name</div>
+                <div class='d-flex'>item-unit</div>
+                <div class='d-flex'>item-quantity</div>
+                <div class='d-flex'>item-price</div>
+                <div class='d-flex'>item-note</div>
+            </div>
         </>
     );
 } 
