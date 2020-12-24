f22d75a9c6c8ffbb9598c7dd1b0a7ff2ab23c5db
lykkell <lykkellit@gmail.com>
2020-12-24 20:46:53 +0200

    class change to className

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -3,14 +3,14 @@ import React from 'react';
 const ListItem = () => {
     return (
         <>
-             <div class='list-item d-flex justify-content-around'>
-                <div class='d-flex'>#00001</div>
-                <button type='button' class='fa fa-checkbox d-flex'></button>
-                <div class='d-flex'>item-short-name</div>
-                <div class='d-flex'>item-unit</div>
-                <div class='d-flex'>item-quantity</div>
-                <div class='d-flex'>item-price</div>
-                <div class='d-flex'>item-note</div>
+             <div className='list-item d-flex justify-content-around'>
+                <div className='d-flex'>#00001</div>
+                <button type='button' className='fa fa-checkbox d-flex'></button>
+                <div className='d-flex'>item-short-name</div>
+                <div className='d-flex'>item-unit</div>
+                <div className='d-flex'>item-quantity</div>
+                <div className='d-flex'>item-price</div>
+                <div className='d-flex'>item-note</div>
             </div>        
         </>
     )
--- src/components/list/list.js
+++ src/components/list/list.js
@@ -6,23 +6,23 @@ import './list.css';
 const List = () => {
     return (
         <>  
-            <div class='d-flex justify-content-between'>
-                <h1 class='d-flex'>Saler: account-name </h1>
-                <h5 class='d-flex'>App: NWITT by Oleksandr Sytnychenko</h5>
+            <div className='d-flex justify-content-between'>
+                <h1 className='d-flex'>Saler: account-name </h1>
+                <h5 className='d-flex'>App: NWITT by Oleksandr Sytnychenko</h5>
             </div>
             
-            <div class='list-header d-flex justify-content-between'>
-                <h2 class='list-title d-flex'>
+            <div className='list-header d-flex justify-content-between'>
+                <h2 className='list-title d-flex'>
                     The List
                 </h2>
-                <h2 class='list-account-name d-flex'>
+                <h2 className='list-account-name d-flex'>
                     Customer: account-name
                 </h2>
             </div>
-            <div class='d-flex justify-content-center' >
-                <div class="dropdown d-flex">
+            <div className='d-flex justify-content-center' >
+                <div className="dropdown d-flex">
                     <button 
-                        class="btn btn-secondary dropdown-toggle" 
+                        className="btn btn-secondary dropdown-toggle" 
                         type="button"       
                         id="dropdownMenu2" 
                         data-toggle="dropdown" 
@@ -30,52 +30,56 @@ const List = () => {
                         aria-expanded="false">
                         Filter
                     </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                        <button class="dropdown-item" type="button">Type of item 1</button>
-                        <button class="dropdown-item" type="button">Type of item 2</button>
-                        <button class="dropdown-item" type="button">Type of item 3</button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                        <button className="dropdown-item" type="button">Type of item 1</button>
+                        <button className="dropdown-item" type="button">Type of item 2</button>
+                        <button className="dropdown-item" type="button">Type of item 3</button>
                     </div>
                 </div>
-                <div class='btn-favorit d-flex'>
-                    <button type='button' class='btn btn-info'>All items</button>
-                    <button type='button' class='btn btn-info'>My favorit items</button>
+                <div className='btn-favorit d-flex'>
+                    <button type='button' className='btn btn-info'>All items</button>
+                    <button type='button' className='btn btn-info'>My favorit items</button>
                 </div>
 
-                <input class='d-flex justify-content-end'
+                <input className='d-flex justify-content-end'
                     className='search-panel form-control search-panel'
                     placeholder='Type for searching...'>
                 </input>
             </div>
-            <h4 class='d-flex justify-content-around'>
-                <div class='product-list-subheader d-flex'>#code</div>
-                <div class='product-list-subheader d-flex'>checkbox</div>
-                <div class='product-list-subheader d-flex'>Colonum-1</div>
-                <div class='product-list-subheader d-flex'>Colonum-2</div>
-                <div class='product-list-subheader d-flex'>Colonum-3</div>
-                <div class='product-list-subheader d-flex'>Colonum-4</div>
-                <div class='product-list-subheader d-flex'>Colonum-5</div>
+            <h4 className='d-flex justify-content-around'>
+                <div className='product-list-subheader d-flex'>#code</div>
+                <div className='product-list-subheader d-flex'>checkbox</div>
+                <div className='product-list-subheader d-flex'>Colonum-1</div>
+                <div className='product-list-subheader d-flex'>Colonum-2</div>
+                <div className='product-list-subheader d-flex'>Colonum-3</div>
+                <div className='product-list-subheader d-flex'>Colonum-4</div>
+                <div className='product-list-subheader d-flex'>Colonum-5</div>
             </h4>
             <ListItem />
             <ListItem />
             <ListItem />
-            <div class="d-flex justify-content-center">
-                <button type="button" class="btn btn-success">Add item to order</button>
-                <button type="button" class="btn btn-success">Add item to my favorits</button>
-                <button type="button" class="btn btn-danger">Delete item from my favorits</button>
-                <a class="btn btn-info" href="#" role="button">Print selected items</a>
-                <a class="btn btn-info" href="#" role="button">Send selected items</a>
+            <div className="d-flex justify-content-center">
+                <button type="button" className="btn btn-success">Add item to order</button>
+                <button type="button" className="btn btn-success">Add item to my favorits</button>
+                <button type="button" className="btn btn-danger">Delete item from my favorits</button>
+                <button className="btn btn-info" type="button">Print selected items
+                    {/* <a href="#"></a> */}
+                </button>
+                <button className="btn btn-info" type="button">Send selected items
+                    {/* <a href="#"></a> */}
+                </button>
             </div>
-            <div class="d-flex justify-content-center">
-                <button type="button" class="btn btn-primary">Add new item to list</button>
-                <button type="button" class="btn btn-danger">Delete item from list</button>
+            <div className="d-flex justify-content-center">
+                <button type="button" className="btn btn-primary">Add new item to list</button>
+                <button type="button" className="btn btn-danger">Delete item from list</button>
             </div>
             <nav aria-label="Page navigation example">
-                <ul class="pagination justify-content-center">
-                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                <ul className="pagination justify-content-center">
+                    <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                    <li className="page-item"><a className="page-link" href="#">1</a></li>
+                    <li className="page-item"><a className="page-link" href="#">2</a></li>
+                    <li className="page-item"><a className="page-link" href="#">3</a></li>
+                    <li className="page-item"><a className="page-link" href="#">Next</a></li>
                 </ul>
             </nav>
         </>
