d109a8ef81d0d5ddbfef9ad77eeae997f11badc0
lykkell <lykkellit@gmail.com>
2022-02-07 18:48:17 +0200

    btn Previos

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code>src/App.js</code>
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/list/list.js
+++ src/components/list/list.js
@@ -75,11 +75,17 @@ const List = () => {
             </div>
             <nav aria-label="Page navigation example">
                 <ul className="pagination justify-content-center">
-                    <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                    <button
+                        className="page-item"
+                        onClick={() => this.setState({ showSomething: true })}
+                        >Previous
+                        {/* {label} */}
+                    </button>
+                    {/* <li className="page-item"><a className="page-link" href="#">Previous</a></li>
                     <li className="page-item"><a className="page-link" href="#">1</a></li>
                     <li className="page-item"><a className="page-link" href="#">2</a></li>
                     <li className="page-item"><a className="page-link" href="#">3</a></li>
-                    <li className="page-item"><a className="page-link" href="#">Next</a></li>
+                    <li className="page-item"><a className="page-link" href="#">Next</a></li> */}
                 </ul>
             </nav>
         </>
